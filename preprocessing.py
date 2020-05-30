4a107984afe1bcf686bb27f2e74f23cc336f70bd
Flurin Gishamer <fgishamer@me.com>
2020-05-30 20:04:03 +0200

    +mix_samples

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,42 @@
+import math
+
+import torch
+import torchaudio
 
 
-def add_noise():
-    pass
+def normalize(tensor: torch.Tensor) -> torch.Tensor:
+    """
+    Subtract the mean, and scale to the interval [-1,1]
+    """
+    tensor_minusmean = tensor - tensor.mean()
+    return tensor_minusmean/tensor_minusmean.abs().max()
+
+
+def match_length(sample: torch.Tensor, length: int) -> torch.Tensor:
+    """
+    Trims an audio-sample to the desired length, if the sample is shorter
+    then the desired length, it will be repeated n times and then trimmed
+    @param sample: audio-sample to be trimmed
+    @param length: desired length
+    @return: trimmed audio-sample
+    """
+    if len(sample) < length:
+        times = math.ceil(length / len(sample))
+        sample = sample.repeat(times)
+    return sample[:length]
+
+
+def mix_samples(sample_a: torch.Tensor, sample_b: torch.Tensor, trim_to: str = "a") -> torch.Tensor:
+    """
+    @param sample_a: first audio-sample
+    @param sample_b: second audio-sample
+    @param trim_to: resulting length will be length of sample_a or sample_b,
+                    if the sample not specified is shorter than the specified one,
+                    it will be repeated to match the specified samples length
+    @return: sum of both samples, normalized to avoid clipping
+    """
+    if trim_to == "b":
+        sample_a, sample_b = sample_b, sample_a
+    sample_b = match_length(sample_b, len(sample_a))
+    add = torch.add(sample_a, sample_b)
+    return normalize(add)
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,7 +1,45 @@
 from unittest import TestCase
-from preprocessing import add_noise
+from preprocessing import match_length, mix_samples
+
+import torch
+import torchaudio
 
 
 class Test(TestCase):
-    def test_add_noise(self):
-        add_noise()
+    def test_match_length(self):
+        sample = torch.ones(4)
+        res = match_length(sample, 3)
+        self.assertEqual(len(res), 3)
+
+    def test_match_length_repeat(self):
+        sample = torch.tensor([.1, .2, .3, .4])
+        res = match_length(sample, 6)
+        print(res)
+        self.assertTrue(torch.equal(res, torch.tensor([.1, .2, .3, .4, .1, .2])))
+
+    def test_mix_samples(self):
+        speech = torch.tensor([-.5, .5, -.5, .5])
+        noise = torch.tensor([-.2, .2, -.2, .2])
+        res = mix_samples(speech, noise)
+        self.assertTrue(torch.equal(res, torch.tensor([-1., 1., -1., 1.])))
+
+    def test_mix_samples_overflow(self):
+        speech = torch.tensor([-.6, .6, -.6, .6])
+        noise = torch.tensor([-.6, .6, -.6, .6])
+        res = mix_samples(speech, noise)
+        self.assertTrue(torch.equal(res, torch.tensor([-1., 1., -1., 1.])))
+
+    def test_mix_samples_trim_to_a(self):
+        speech = torch.tensor([-.2, .2, -.2, .2])
+        noise = torch.tensor([-.6, .6])
+        res = mix_samples(speech, noise)
+        self.assertTrue(torch.equal(res, torch.tensor([-1., 1., -1., 1.])))
+
+    def test_mix_samples_trim_to_b(self):
+        speech = torch.tensor([-.2, .2, -.2, .2])
+        noise = torch.tensor([-.6, .6])
+        res = mix_samples(speech, noise, "b")
+        print(res)
+        self.assertTrue(torch.equal(res, torch.tensor([-1., 1.])))
+
+
