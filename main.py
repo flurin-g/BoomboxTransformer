dd8109697efb77d8a4915d8be1f57f91681d6c86
Flurin Gishamer <fgishamer@me.com>
2020-06-02 07:03:17 +0200

    +arg-parse and create-meta

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import pandas as pd
 from utils import create_data_frame
 
@@ -7,7 +8,27 @@ def create_libri_meta(libri_path: str, libri_meta_path: str, file_name: str = "l
     libri_df.to_csv(path_or_buf=file_name, index=False)
 
 
-if __name__ == "__main__":
-    # ToDo: include arg-parse
-    create_libri_meta(libri_path="data/LibriSpeech",
-                      libri_meta_path="data/LibriSpeech/SPEAKERS.TXT")
+def parse_args(parser):
+    """
+    Parse commandline arguments.
+    """
+
+    parser.add_argument('--task', type=str, choices=['train', 'create-meta'], required=True,
+                        help='Choose task to run')
+
+    return parser
+
+
+def main():
+    parser = argparse.ArgumentParser(description='PyTorch BoomboxTransformer')
+    parser = parse_args(parser)
+    args, unknown_args = parser.parse_known_args()
+
+    if "create-meta" in args.task:
+        create_libri_meta(libri_path="data/LibriSpeech",
+                          libri_meta_path="data/LibriSpeech/SPEAKERS.TXT")
+
+
+if __name__ == '__main__':
+    main()
+
