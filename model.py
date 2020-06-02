6ef60c3690882d8183525cbedaad5d6ce1a2e9e9
Flurin Gishamer <fgishamer@me.com>
2020-06-02 07:03:41 +0200

    +pytorch-lightning integration

---
+++ model.py
@@ -0,0 +1,13 @@
+import torch
+from torch.nn import functional as F
+from torch import nn
+from pytorch_lightning.core.lightning import LightningModule
+
+
+class BoomboxTransformer(LightningModule):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+    
