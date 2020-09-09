e9dd7398f659f560db167ad321deeaaae43c3029
Flurin Gishamer <fgishamer@me.com>
2020-09-09 10:45:43 +0200

    +tensorboard logger

--- trainer_main.py
+++ trainer_main.py
@@ -6,20 +6,22 @@ import pathlib
 import hydra
 from omegaconf import DictConfig
 from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
 
 from model import BoomboxTransformer
 
 CWD = pathlib.Path(__file__).parent.absolute()
 
 
-@hydra.main(config_path=".", config_name="parameters.yml")
+@hydra.main(config_path="conf", config_name="config")
 def my_app(cfg: DictConfig) -> None:
+    tb_logger = TensorBoardLogger(save_dir=cfg.save_dir)
     boom: BoomboxTransformer = BoomboxTransformer(cfg, CWD)
-    # print(cfg.pretty()) to view the parameters
+    # print(cfg.pretty()) #to view the parameters
     if cfg.create_meta:
         boom.prepare_data()
 
-    trainer = Trainer()
+    trainer = Trainer(logger=tb_logger)
     trainer.fit(boom)
 
 
